b92fdf22b932800419d1317e0c1c10fad417cdf6
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-07-19 21:57:06 +0000

    Just saving what I have so far.

--- Image_Blur_1.rb
+++ Image_Blur_1.rb
@@ -1,1 +1,32 @@
-puts 'Hello world!'
+class Image
+  def initialize
+    image = []
+    
+    row1 = [0, 0, 0, 0]
+    row2 = [0, 1, 0, 0]
+    row3 = [0, 0, 0, 1]
+    row4 = [0, 0, 0, 0]
+  end
+
+  image << row1
+  image << row2
+  image << row3
+  image << row4
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
